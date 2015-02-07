c3bbf5268619e5c88a31e8bef29f68fd532843a5
Fran Ramirez <jframonge@gmail.com>
2015-02-07 00:35:56 -0600

    First round

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function (){
+  var latlng = new google.maps.LatLng(41.3947901, 2.1487678);
+  var options = {
+    zoom: 15,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    navigationControl: true,
+    mapTypeControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true
+  };
+
+  var map = new google.maps.Map(document.getElementById('map'), options);
+
+  var marker1 = new google.maps.Marker({
+    position: latlng, map: map
+  });
+
+  google.maps.event.addListener(marker1, 'click', function() {
+    infowindow.open(map, marker1);
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+    content:  '<div class="info"><strong></strong><br><br>Event address here<br> Orcid rules!</div>'
+  });
+
+  new WOW().init();
+
+});
