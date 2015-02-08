49dfeb85217fca34341eaea8a61fedbfa7498ca4
rcpeters <info@rcpeters.com>
2015-02-07 16:31:37 -0800

    add links and address

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
-  var latlng = new google.maps.LatLng(41.3947901, 2.1487678);
+  var latlng = new google.maps.LatLng(41.386697,2.163923);
   var options = {
     zoom: 15,
     center: latlng,
@@ -20,10 +20,14 @@ $(document).ready(function (){
     infowindow.open(map, marker1);
   });
 
+
   var infowindow = new google.maps.InfoWindow({
-    content:  '<div class="info"><strong></strong><br><br>Event address here<br> Orcid rules!</div>'
+    content:  '<div class="info"><strong>Universitat de Barcelona</strong><br />' 
+    + '<br/ >Gran Via de les Corts Catalanes, 585<br />08007 Barcelona<br> Building a room TBD</div>'
   });
 
+  infowindow.open(map, marker1);
+  
   new WOW().init();
 
   var scrollIt = function(){
